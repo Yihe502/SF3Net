87d34d93c752584b73a21512050965daed6ae1a6
Yihe502 <yihe5cver@gmail.com>
2025-09-18 15:18:33 +0800

    Update __init__.py

--- mmseg/decode_heads/__init__.py
+++ mmseg/decode_heads/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
-from .lfsfunet_head import FSFDecodeHead_T
+from .lfsfunet_head import SF3DecodeHead_T
 __all__ = [
-    'FSFDecodeHead_T', 'FSFDecodeHead_S', 'FSFDecodeHead_B', 'FSFDecodeHead_L'
+    'SF3DecodeHead_T', 'SF3DecodeHead_S', 'SF3DecodeHead_B', 'SF3DecodeHead_L'
 
 ]
